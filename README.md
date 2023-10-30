6eb106926ec993a9cf54899c53aabd243e2a00e4
rcx69 <59633184+p978@users.noreply.github.com>
2023-10-30 20:46:26 +0545

    Added a readme

---
+++ README.md
@@ -0,0 +1,5 @@
+#Torrent-Search
+This is a command line program to find torrents in 1337x.to.
+
+#Options
+`--search or -s`
--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import sys
 
 if sys.argv[1] == "-s" or sys.argv[1] == "--search":
     torrent_query = sys.argv[2]
-    torrents = py1337x(proxy='1337x.to', cache='py1337xCache', cacheTime=500) 
-    results = torrents.search(torrent_query)
-    print(type(results))
+    if "" in torrent_query:
+        torrents = py1337x(proxy='1337x.to', cache='py1337xCache', cacheTime=500) 
+        results = torrents.search(torrent_query) 
+        for i,j in enumerate(results.get("items")): #type: ignore
+            print(i,f".{j}" ,end="\n")
+    else:
+        print("Please add quotation marks in the search query")

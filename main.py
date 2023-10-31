c961319866556b6cac574a79ec49ea81a9a92258
rcx69 <59633184+p978@users.noreply.github.com>
2023-10-31 09:12:48 +0545

    main.py

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ if sys.argv[1] == "-s" or sys.argv[1] == "--search":
     if "" in torrent_query:
         torrents = py1337x(proxy='1337x.to', cache='py1337xCache', cacheTime=500) 
         results = torrents.search(torrent_query) 
+        if results.get("itemCount") == 0:
+            print("The torrent you are searching for doesn't exist")
         for i,j in enumerate(results.get("items")): #type: ignore
             print(i,j.get("name"))
-            if sys.argv[3] == str(i):
-                print(j.get("link")) 
+            try:
+                if sys.argv[3] == str(i):
+                    print(j.get("link")) 
+            except Exception:
+                pass
+                
     else:
         print("Please add quotation marks in the search query")
